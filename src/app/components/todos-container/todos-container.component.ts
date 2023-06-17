0b989ca35fe0ce509741a4d563597c41cc4a695c
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-06-16 21:28:57 -0600

    working app

--- src/app/components/todos-container/todos-container.component.ts
+++ src/app/components/todos-container/todos-container.component.ts
@@ -1,77 +1,71 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Signal,
   computed,
   signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem,
-  CdkDrag,
-  CdkDropList,
-} from '@angular/cdk/drag-drop';
+const createRandomId = () => Math.round(Math.random() * 10000).toString();
 
-type Todo = {
+import { FormsModule } from '@angular/forms';
+
+type Task = {
   name: string;
   completed: boolean;
+  id: string;
 };
 
 @Component({
   selector: 'app-todos-container',
   standalone: true,
-  imports: [CommonModule, CdkDropList, CdkDrag],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todos-container.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styles: [
-    `
-      .cdk-drag-placeholder {
-        opacity: 0;
-      }
-
-      .cdk-drag-animating {
-        transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
-      }
-      .example-box:last-child {
-        border: none;
-      }
-
-      .example-list.cdk-drop-list-dragging
-        .example-box:not(.cdk-drag-placeholder) {
-        transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
-      }
-    `,
-  ],
+  styles: [],
 })
 export class TodosContainerComponent {
-  tasks = signal([
-    { name: 'Get to work', completed: false },
-    { name: 'Pick up groceries', completed: false },
-    { name: 'Go home', completed: false },
-    { name: 'Fall asleep', completed: false },
-  ]);
+  inputSignal = signal('');
+  tasks = signal<Task[]>([]);
 
-  done = computed(() => this.tasks().filter((t) => t.completed));
-  todos = computed(() => this.tasks().filter((t) => !t.completed));
+  done = computed(() => this.tasks().filter((t: Task) => t?.completed));
+  todos = computed(() => this.tasks().filter((t: Task) => !t?.completed));
+  totals = computed(() => {
+    return `${this.done().length} / ${this.tasks().length}`;
+  });
 
-  drop(event: CdkDragDrop<Todo[]>) {
-    console.log('event: ', event);
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    }
+  addTodo() {
+    this.tasks.mutate((tasks) =>
+      tasks.push({
+        name: this.inputSignal(),
+        completed: false,
+        id: createRandomId(),
+      })
+    );
+    this.inputSignal.set('');
+  }
+
+  removeTodo(task: Task) {
+    this.tasks.update(
+      (tasks) => (tasks = tasks.filter((t) => t.id !== task.id))
+    );
+  }
+
+  completeTodo(task: Task) {
+    this.tasks.mutate((tasks) => {
+      const taskCompleted = tasks.find((t) => t.id === task.id);
+      if (taskCompleted) {
+        taskCompleted.completed = true;
+      }
+    });
+  }
+
+  markTodoAsIncomplete(task: Task) {
+    this.tasks.mutate((tasks) => {
+      const taskCompleted = tasks.find((t) => t.id === task.id);
+      if (taskCompleted) {
+        taskCompleted.completed = false;
+      }
+    });
   }
 }
